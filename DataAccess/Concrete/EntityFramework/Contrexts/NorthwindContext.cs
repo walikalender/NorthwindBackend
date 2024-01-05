f0cf6abf6a7bd91cee13fd29ef58c93074f10594
muhammed ali kalender <walikalender@gmail.com>
2024-01-06 01:33:27 +0300

    + Coding the Northwind Context Object + ConnectionString configured

---
+++ DataAccess/Concrete/EntityFramework/Contrexts/NorthwindContext.cs
@@ -0,0 +1,20 @@
+﻿using Entities.Concrete;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.Concrete.EntityFramework.Contrexts
+{
+    public class NorthwindContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(connectionString: @"Server=(localdb)\MSSQLLocalDB;Database:Northwind;Trusted_Connection=true");
+        }
+
+        public DbSet<Product> Products { get; set; }
+    }
+}
