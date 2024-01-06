0f50ce94bbfc283c523d1baac2248d522174d930
muhammed ali kalender <walikalender@gmail.com>
2024-01-07 01:43:39 +0300

    + Category Entity Coded in Business Layer + ICategoryService Created, Method Signatures Written + CategoryManager has been created and implemented by ICategoryService

---
+++ Business/Constants/Messages/CategoryMessages.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.Constants.Messages
+{
+    public static class CategoryMessages
+    {
+        public static string CategoryAdded = "Kategori Başarıyla Eklendi";
+        public static string CategoryDeleted = "Kategori Başarıyla Silindi";
+        public static string CategoryUpdated = "Kategori Başarıyla Güncellendi";
+        public static string CategoriesListed = "Kategoriler Başarıyla Listelendi";
+        public static string CategoryGet = "Kategori Başarıyla Getirildi";
+    }
+}
