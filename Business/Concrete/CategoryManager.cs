83544fc9a01f7f601cab407dc8203559e7590c99
muhammed ali kalender <walikalender@gmail.com>
2024-01-07 01:43:39 +0300

    + Category Entity Coded in Business Layer + ICategoryService Created, Method Signatures Written + CategoryManager has been created and implemented by ICategoryService + DependencyInjection Configured in CategoryManger + CategoryMessages created in Constant

---
+++ Business/Concrete/CategoryManager.cs
@@ -0,0 +1,48 @@
+﻿using Business.Abstract;
+using Business.Constants.Messages;
+using Core.Utilities.Results;
+using DataAccess.Abstract;
+using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.Concrete
+{
+    public class CategoryManager(ICategoryDal categoryDal) : ICategoryService
+    {
+        private readonly ICategoryDal _categoryDal = categoryDal;
+
+        public IResult Add(Category category)
+        {
+            _categoryDal.Add(category);
+            return new SuccessResult(CategoryMessages.CategoryAdded);
+        }
+
+        public IResult Delete(Category category)
+        {
+            _categoryDal.Delete(category);
+            return new SuccessResult(CategoryMessages.CategoryDeleted);
+        }
+
+        public IDataResult<Category> GetById(int categoryId)
+        {
+            var result = _categoryDal.Get(c => c.CategoryID==categoryId);
+            return new SuccessDataResult<Category>(result, CategoryMessages.CategoryGet);
+        }
+
+        public IDataResult<List<Category>> GetList()
+        {
+            var result = _categoryDal.GetList().ToList();
+            return new SuccessDataResult<List<Category>>(result, CategoryMessages.CategoriesListed);
+        }
+
+        public IResult Update(Category category)
+        {
+            _categoryDal.Update(category);
+            return new SuccessResult(CategoryMessages.CategoryUpdated);
+        }
+    }
+}
