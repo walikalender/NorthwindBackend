ff2a09400b5880f2eedbb3e20c7180945f72cd5d
muhammed ali kalender <walikalender@gmail.com>
2024-01-07 19:46:58 +0300

    + User Entity Coded in Business Layer + IUserService Created, Method Signatures Written + UserManager has been created and implemented by IUserService + DependencyInjection Configured in UserManager + UserMessages created in Constant + AutofacBusinessModule Configured for User Object

---
+++ Business/Abstract/IUserService.cs
@@ -0,0 +1,21 @@
+﻿using Core.Utilities.Results;
+using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.Abstract
+{
+    public interface IUserService
+    {
+        IDataResult<List<OperationClaim>> GetOperationClaims(User user);
+        IDataResult<List<User>> GetList();
+        IResult Add(User user);
+        IResult Delete(User user);
+        IResult Update(User user);
+        IDataResult<User> GetById(int userId);
+        IDataResult<User> GetByMail(string mail);
+    }
+}
---
+++ Business/Concrete/UserManager.cs
@@ -0,0 +1,59 @@
+﻿using Business.Abstract;
+using Business.Constants.Messages;
+using Core.Utilities.Results;
+using DataAccess.Abstract;
+using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.Concrete
+{
+    public class UserManager(IUserDal userDal) : IUserService
+    {
+        private readonly IUserDal _userDal = userDal;
+        public IResult Add(User user)
+        {
+            _userDal.Add(user);
+            return new SuccessResult(UserMessages.UserAdded);
+        }
+
+        public IResult Delete(User user)
+        {
+            _userDal.Delete(user);
+            return new SuccessResult(UserMessages.UserDeleted);
+        }
+
+        public IDataResult<User> GetById(int userId)
+        {
+            var result = _userDal.Get(u => u.Id==userId);
+            return new SuccessDataResult<User>(result, UserMessages.UserGet);
+        }
+
+        public IDataResult<User> GetByMail(string mail)
+        {
+            var result = _userDal.Get(u => u.Email==mail);
+            return new SuccessDataResult<User>(result, UserMessages.UserGet);
+        }
+
+        public IDataResult<List<User>> GetList()
+        {
+            var result = _userDal.GetList().ToList();
+            return new SuccessDataResult<List<User>>(result, UserMessages.UsersListed);
+        }
+
+        public IDataResult<List<OperationClaim>> GetOperationClaims(User user)
+        {
+            var result = _userDal.GetClaims(user).ToList();
+            return new SuccessDataResult<List<OperationClaim>>(result, UserMessages.UsersClaimsListed);
+        }
+
+        public IResult Update(User user)
+        {
+            _userDal.Update(user);
+            return new SuccessResult(UserMessages.UserUpdated);
+        }
+    }
+}
