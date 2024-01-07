e1291688b7a995fe1fac6876753e55e22282fd37
muhammed ali kalender <walikalender@gmail.com>
2024-01-07 19:29:35 +0300

    + Created IUserDal, EfUserDal in DataAccess Layer + Added GetClaims method to IUserDal + IUserDal implement for EfUserDal. + Joined OperationClaim with UserOperationClaim for GetClaims

--- DataAccess/Concrete/EntityFramework/Contrexts/NorthwindContext.cs
+++ DataAccess/Concrete/EntityFramework/Contrexts/NorthwindContext.cs
@@ -17,5 +17,8 @@ namespace DataAccess.Concrete.EntityFramework.Contrexts
 
         public DbSet<Product> Products { get; set; }
         public DbSet<Category> Categories { get; set; }
+        public DbSet<User> Users { get; set; }
+        public DbSet<OperationClaim> OperationClaims { get; set; }
+        public DbSet<UserOperationClaim> UserOperationClaims { get; set; }
     }
 }
---
+++ DataAccess/Concrete/EntityFramework/EfUserDal.cs
@@ -0,0 +1,33 @@
+﻿using Core.DataAccess.EntityFramework;
+using DataAccess.Abstract;
+using DataAccess.Concrete.EntityFramework.Contrexts;
+using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.Concrete.EntityFramework
+{
+    public class EfUserDal : EfEntityRepositoryBase<User, NorthwindContext>, IUserDal
+    {
+        public List<OperationClaim> GetClaims(User user)
+        {
+            using (var context = new NorthwindContext())
+            {
+                var result = from userOperationClaim in context.UserOperationClaims
+                             join operationClaim in context.OperationClaims
+                             on userOperationClaim.OperationClaimId equals operationClaim.Id
+                             where userOperationClaim.UserId==user.Id
+                             select new OperationClaim
+                             {
+                                 Id = operationClaim.Id,
+                                 Name=operationClaim.Name
+                             };
+                return result.ToList();
+            }
+
+        }
+    }
+}
