bf0b75fb4eb8ecae18f3bb1a04f7a950c9318b1a
muhammed ali kalender <walikalender@gmail.com>
2024-01-14 15:30:30 +0300

    + + Fixed Messages class name fixed

--- Business/Constants/Messages/CategoryMessages.cs
+++ Business/Constants/Messages/CategoryMessages.cs
@@ -6,13 +6,12 @@ using System.Threading.Tasks;
 
 namespace Business.Constants.Messages
 {
-    public static class UserMessages
+    public static class CategoryMessages
     {
-        public static string UserAdded = "Kullanıcı Başarıyla Eklendi";
-        public static string UserDeleted = "Kullanıcı Başarıyla Silindi";
-        public static string UserUpdated = "Kullanıcı Başarıyla Güncellendi";
-        public static string UsersListed = "Kullanıcıler Başarıyla Listelendi";
-        public static string UsersClaimsListed = "Kullanıcıler Rolleri Başarıyla Listelendi";
-        public static string UserGet = "Kullanıcı Başarıyla Getirildi";
+        public static string CategoryAdded = "Kategori Başarıyla Eklendi";
+        public static string CategoryDeleted = "Kategori Başarıyla Silindi";
+        public static string CategoryUpdated = "Kategori Başarıyla Güncellendi";
+        public static string CategoriesListed = "Kategoriler Başarıyla Listelendi";
+        public static string CategoryGet = "Kategori Başarıyla Getirildi";
     }
 }
--- Business/Constants/Messages/UserMessages.cs
+++ Business/Constants/Messages/UserMessages.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Core.Entities.Concrete;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,12 +7,14 @@ using System.Threading.Tasks;
 
 namespace Business.Constants.Messages
 {
-    public static class CategoryMessages
+    public static class UserMessages
     {
-        public static string CategoryAdded = "Kategori Başarıyla Eklendi";
-        public static string CategoryDeleted = "Kategori Başarıyla Silindi";
-        public static string CategoryUpdated = "Kategori Başarıyla Güncellendi";
-        public static string CategoriesListed = "Kategoriler Başarıyla Listelendi";
-        public static string CategoryGet = "Kategori Başarıyla Getirildi";
+        public static string UserAdded = "Kullanıcı Başarıyla Eklendi";
+        public static string UserDeleted = "Kullanıcı Başarıyla Silindi";
+        public static string UserUpdated = "Kullanıcı Başarıyla Güncellendi";
+        public static string UsersListed = "Kullanıcıler Başarıyla Listelendi";
+        public static string UsersClaimsListed = "Kullanıcıler Rolleri Başarıyla Listelendi";
+        public static string UserGet = "Kullanıcı Başarıyla Getirildi";
+        public static string UserNotFound = "Böyle bir kullanıcı mevcut değil";
     }
 }
