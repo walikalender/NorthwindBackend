c159193567b935fe2f7e48b99a971a27c3bbf9f4
Muhammed Ali KALENDER <walikalender@gmail.com>
2024-02-17 04:23:17 +0300

    + LoggerService Config Configurated

---
+++ Core/CrossCuttingConcerns/Logging/Log4Net/LoggerServiceBase.cs
@@ -0,0 +1,28 @@
+﻿using log4net;
+using log4net.Repository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+
+namespace Core.CrossCuttingConcerns.Logging.Log4Net
+{
+    public class LoggerServiceBase
+    {
+        ILog _logger;
+        public LoggerServiceBase(string name)
+        {
+            XmlDocument xmlDocument = new XmlDocument();
+            xmlDocument.Load(File.OpenRead(path: "log4net.config"));
+
+            ILoggerRepository loggerRepository = LogManager.CreateRepository(Assembly.GetEntryAssembly(), typeof(log4net.Repository.Hierarchy.Hierarchy));
+
+            log4net.Config.XmlConfigurator.Configure(loggerRepository, xmlDocument["log4net"]);
+
+            _logger = LogManager.GetLogger(loggerRepository.Name, name);
+        }
+    }
+}
