423b36e74bc1a4dc5e8bd0931dbb864ab2e3a3f9
Muhammed Ali KALENDER <walikalender@gmail.com>
2024-02-17 04:28:08 +0300

    + LoggerService Implementation

--- Core/CrossCuttingConcerns/Logging/Log4Net/LoggerServiceBase.cs
+++ Core/CrossCuttingConcerns/Logging/Log4Net/LoggerServiceBase.cs
@@ -24,5 +24,45 @@ namespace Core.CrossCuttingConcerns.Logging.Log4Net
 
             _logger = LogManager.GetLogger(loggerRepository.Name, name);
         }
+        public bool IsInfoEnabled => _logger.IsInfoEnabled;
+        public bool IsDebugEnabled => _logger.IsDebugEnabled;
+        public bool IsWarnEnabled => _logger.IsWarnEnabled;
+        public bool IsFatalEnabled => _logger.IsFatalEnabled;
+        public bool IsErrorEnabled => _logger.IsErrorEnabled;
+        public void Info(object logMessage)
+        {
+            if (_logger.IsInfoEnabled)
+            {
+                _logger.Info(logMessage);
+            }
+        }
+        public void Debug(object logMessage)
+        {
+            if (_logger.IsInfoEnabled)
+            {
+                _logger.Debug(logMessage);
+            }
+        }
+        public void Warn(object logMessage)
+        {
+            if (_logger.IsInfoEnabled)
+            {
+                _logger.Warn(logMessage);
+            }
+        }
+        public void Fatal(object logMessage)
+        {
+            if (_logger.IsInfoEnabled)
+            {
+                _logger.Fatal(logMessage);
+            }
+        }
+        public void Error(object logMessage)
+        {
+            if (_logger.IsInfoEnabled)
+            {
+                _logger.Error(logMessage);
+            }
+        }
     }
 }
